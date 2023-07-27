f9545a102147c45897770144291f552ae2072c74
jamiukabir22 <kabirm474@gmail.com>
2023-07-27 15:08:16 -0700

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Create function to generate tgx file"""
+"""Create funtion for generate tgz file."""
 from fabric.api import local
 import time
 
 
 def do_pack():
-    """Generates tgz"""
+    """Gerenate tgz."""
     timestamp = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar-cvzf versions/web_static_{:s}.tgz web_static/".
-                format(timestamp))
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              format(timestamp))
         return ("versions/web_static_{:s}.tgz".format(timestamp))
     except FileNotFoundError:
         return None
